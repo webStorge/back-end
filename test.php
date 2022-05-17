7c56f17f7d9a456101fab9dd86ceed90cb2bf95e
park sang yong <ys3380@naver.com>
2022-05-18 00:53:30 +0900

    upload

---
+++ test.php
@@ -0,0 +1,43 @@
+<?php
+ini_set("display_errors", 'on');
+error_reporting(E_ALL);
+$file_path = realpath(__FILE__);
+echo $file_path;
+require 'vendor/autoload.php';
+$storage = new \Upload\Storage\FileSystem('saveFile');
+$file = new \Upload\File('foo', $storage);
+
+// Optionally you can rename the file on upload
+$new_filename = uniqid();
+$file->setName($new_filename);
+
+// Validate file upload
+// MimeType List => http://www.iana.org/assignments/media-types/media-types.xhtml
+$file->addValidations(array(
+    // Ensure file is of type "image/png"
+    new \Upload\Validation\Mimetype('image/png'),
+
+    //You can also add multi mimetype validation
+    //new \Upload\Validation\Mimetype(array('image/png', 'image/gif'))
+
+    // Ensure file is no larger than 5M (use "B", "K", M", or "G")
+    new \Upload\Validation\Size('5M')
+));
+
+// Access data about the file that has been uploaded
+$data = array(
+    'name'       => $file->getNameWithExtension(),
+    'extension'  => $file->getExtension(),
+    'mime'       => $file->getMimetype(),
+    'size'       => $file->getSize()
+);
+
+// Try to upload file
+try {
+    // Success!
+    $file->upload();
+} catch (\Exception $e) {
+    // Fail!
+    $errors = $file->getErrors();
+}
+?>
